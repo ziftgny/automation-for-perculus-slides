275a1993fdfacbefc0eb91a6c1d4b97ff3986e19
ziftgny <114746717+ziftgny@users.noreply.github.com>
2023-03-01 14:40:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if(window.addEventListener) {
    }
    function contentChanged() {
     //sleep and click on the download button
-    sleep(1000).then(() => {
+    sleep(250).then(() => {
     download_button[0].click();
     });
  
