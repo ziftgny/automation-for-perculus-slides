1a6f63d658ffe61062b97f9de79e8dded098b599
ziftgny <114746717+ziftgny@users.noreply.github.com>
2023-03-01 14:01:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//function for sleeping
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+//we get the div element which will change when teacher goes through another page of the pdf
+var changing_value = document.getElementsByClassName("board-pager-menu-item current-page");
+//get the download button
+var download_button = document.getElementsByClassName("pointer btn-event download-image");
+//adding event listeners
+if(window.addEventListener) {
+   changing_value[0].addEventListener('DOMSubtreeModified', contentChanged, false);
+} else
+   if(window.attachEvent) {
+      changing_value[0].attachEvent('DOMSubtreeModified', contentChanged);
+   }
+   function contentChanged() {
+    //sleep and click on the download button
+    sleep(3000).then(() => {
+    download_button[0].click();
+    });
+ 
+}
